d1db87692bac8de54a746905f2faf2d180653fa2
Aylei <rayingecho@gmail.com>
2021-01-16 12:42:59 +0800

    Disable signal handler
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- cmd/src/signal_handler.rs
+++ cmd/src/signal_handler.rs
@@ -5,33 +5,30 @@ pub use self::imp::wait_for_signal;
 #[cfg(unix)]
 mod imp {
     use engine_rocks::RocksEngine;
-    use engine_traits::{Engines, MiscExt, RaftEngine};
-    use libc::c_int;
-    use nix::sys::signal::{SIGHUP, SIGINT, SIGTERM, SIGUSR1, SIGUSR2};
-    use signal::trap::Trap;
-    use tikv_util::metrics;
+    use engine_traits::{Engines, RaftEngine};
 
     #[allow(dead_code)]
-    pub fn wait_for_signal<ER: RaftEngine>(engines: Option<Engines<RocksEngine, ER>>) {
-        let trap = Trap::trap(&[SIGTERM, SIGINT, SIGHUP, SIGUSR1, SIGUSR2]);
-        for sig in trap {
-            match sig {
-                SIGTERM | SIGINT | SIGHUP => {
-                    info!("receive signal {}, stopping server...", sig as c_int);
-                    break;
-                }
-                SIGUSR1 => {
-                    // Use SIGUSR1 to log metrics.
-                    info!("{}", metrics::dump());
-                    if let Some(ref engines) = engines {
-                        info!("{:?}", MiscExt::dump_stats(&engines.kv));
-                        info!("{:?}", RaftEngine::dump_stats(&engines.raft));
-                    }
-                }
-                // TODO: handle more signal
-                _ => unreachable!(),
-            }
-        }
+    pub fn wait_for_signal<ER: RaftEngine>(_: Option<Engines<RocksEngine, ER>>) {
+        loop {}
+        // let trap = Trap::trap(&[SIGTERM]);
+        // for sig in trap {
+        //     match sig {
+        //         SIGTERM => {
+        //             info!("receive signal {}, stopping server...", sig as c_int);
+        //             break;
+        //         }
+        //         SIGUSR1 => {
+        //             // Use SIGUSR1 to log metrics.
+        //             info!("{}", metrics::dump());
+        //             if let Some(ref engines) = engines {
+        //                 info!("{:?}", MiscExt::dump_stats(&engines.kv));
+        //                 info!("{:?}", RaftEngine::dump_stats(&engines.raft));
+        //             }
+        //         }
+        //         // TODO: handle more signal
+        //         _ => unreachable!(),
+        //     }
+        // }
     }
 }
 
